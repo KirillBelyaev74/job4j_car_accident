61a03cc0bfb5cd19d3be11047b62553699288fee
kirillbelyaev74 <kirbel74@gmail.com>
2021-05-19 21:48:52 +0500

    0. Spring DataSource[6878#449988]

--- src/main/java/ru/job4j/accident/repository/AccidentJdbc.java
+++ src/main/java/ru/job4j/accident/repository/AccidentJdbc.java
@@ -9,7 +9,7 @@ import ru.job4j.accident.model.Rule;
 
 import java.util.*;
 
-//@Repository
+@Repository
 public class AccidentJdbc {
 
     private final JdbcTemplate jdbc;
@@ -32,25 +32,49 @@ public class AccidentJdbc {
     }
 
     public List<Accident> getAllAccident() {
-        return jdbc.query("select * from job4j_cars_accident.public.accident order by id",
+        return jdbc.query("select distinct a.id aId, a.name aName, a.text aText, a.address aAddress, t.id tId, t.name tName, r.id rId, r.name rName"
+                        + " from job4j_cars_accident.public.accident as a"
+                        + " left join job4j_cars_accident.public.accident_type as t on(a.id_accident_type = t.id)"
+                        + " left join job4j_cars_accident.public.accident_rule as ar on(ar.id_accident = a.id)"
+                        + " left join job4j_cars_accident.public.rule as r on (r.id = ar.id_rule) order by a.id",
                 (result, row) -> {
+                    List<Rule> rules = new ArrayList<>();
+                    AccidentType accidentType = new AccidentType(result.getString("tName"));
+                    accidentType.setId(result.getInt("tId"));
+                    Rule rule = new Rule(result.getString("rName"));
+                    rule.setId(result.getInt("rId"));
+                    rules.add(rule);
                     Accident accident = new Accident(
-                            result.getString("name"),
-                            result.getString("text"),
-                            result.getString("address"));
-                    accident.setId(result.getInt("id"));
+                            result.getString("aName"),
+                            result.getString("aText"),
+                            result.getString("aAddress"));
+                    accident.setId(result.getInt("aId"));
+                    accident.setAccidentType(accidentType);
+                    accident.setRules(rules);
                     return accident;
                 });
     }
 
     public Accident getAccidentId(int id) {
-        return jdbc.queryForObject("select * from job4j_cars_accident.public.accident where id = ?",
+        return jdbc.queryForObject("select distinct a.id aId, a.name aName, a.text aText, a.address aAddress, t.id tId, t.name tName, r.id rId, r.name rName"
+                        + " from job4j_cars_accident.public.accident as a"
+                        + " left join job4j_cars_accident.public.accident_type as t on(a.id_accident_type = t.id)"
+                        + " left join job4j_cars_accident.public.accident_rule as ar on(ar.id_accident = a.id)"
+                        + " left join job4j_cars_accident.public.rule as r on (r.id = ar.id_rule) where a.id = ?",
                 (result, row) -> {
+                    List<Rule> rules = new ArrayList<>();
+                    AccidentType accidentType = new AccidentType(result.getString("tName"));
+                    accidentType.setId(result.getInt("tId"));
+                    Rule rule = new Rule(result.getString("rName"));
+                    rule.setId(result.getInt("rId"));
+                    rules.add(rule);
                     Accident accident = new Accident(
-                            result.getString("name"),
-                            result.getString("text"),
-                            result.getString("address"));
-                    accident.setId(result.getInt("id"));
+                            result.getString("aName"),
+                            result.getString("aText"),
+                            result.getString("aAddress"));
+                    accident.setId(result.getInt("aId"));
+                    accident.setAccidentType(accidentType);
+                    accident.setRules(rules);
                     return accident;
                 }, id);
     }
